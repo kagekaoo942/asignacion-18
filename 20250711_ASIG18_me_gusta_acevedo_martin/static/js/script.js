4ec5dd77a8b747613d9c0f524a9075954c6fc046
martin <martinacevedo@liceovvh.cl>
2025-07-11 12:36:29 -0400

    terminar

--- 20250711_ASIG18_me_gusta_acevedo_martin/static/js/script.js
+++ 20250711_ASIG18_me_gusta_acevedo_martin/static/js/script.js
@@ -1,9 +1,15 @@
 function darLike1(){
-let meGusta 
+    const spanLike1 = document.querySelector("#like1");
+    let cantidad = parseInt(spanLike1.innerText);
+    spanLike1.innerText = cantidad + 1;
 }
 function darLike2(){
-
+  const spanLike2 = document.querySelector("#like2");
+    let cantidad = parseInt(spanLike2.innerText);
+    spanLike2.innerText = cantidad + 1;
 }
 function darLike3(){
-
+  const spanLike3 = document.querySelector("#like3");
+    let cantidad = parseInt(spanLike3.innerText);
+    spanLike3.innerText = cantidad + 1;
 }
